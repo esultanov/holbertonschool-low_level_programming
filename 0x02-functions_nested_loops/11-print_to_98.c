6dadbb1bab5c8d5469176fe2c6e150b7a6f834e1
Elnur Sultanov <esultanov6@gmail.com>
2019-09-25 01:14:04 +0000

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all numbers till.
+ * @n: input integer.
+ * Return: summ.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
