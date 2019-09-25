841dd47f584a24a33f8421afbe63821696b76402
Elnur Sultanov <esultanov6@gmail.com>
2019-09-25 00:40:23 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - prints last digit of the int.
+ * @n: number input.
+ * @m: number input.
+ * Return: summ.
+ */
+int add(int n, int m)
+{
+	int s;
+
+	s = n + m;
+	return (s);
+}
