cc99d308437d082c55eb0a75bde5e69089f40605
Elnur Sultanov <esultanov6@gmail.com>
2019-09-26 19:50:57 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies a and b value.
+ * @a: character input.
+ * @b: input.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int summ;
+
+	summ = a * b;
+	return (summ);
+}
