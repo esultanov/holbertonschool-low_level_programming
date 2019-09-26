afc788f1742793e116a0f156c4f257d7ca4c89a6
Elnur Sultanov <esultanov6@gmail.com>
2019-09-26 20:46:08 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print from 0 to 9..
+ * character input.
+ * Return: Always numbers.
+ */
+void print_most_numbers(void)
+{
+	int nums;
+
+	nums = '0';
+	while (nums <= '9')
+	{
+		if (nums != 2 && nums != 4)
+			_putchar(nums);
+		nums++;
+	}
+	_putchar('\n');
+}
