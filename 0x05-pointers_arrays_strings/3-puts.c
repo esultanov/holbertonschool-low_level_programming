8365da517b975f213a25e0a2f788bbed6bd85930
Elnur Sultanov <esultanov6@gmail.com>
2019-10-01 15:18:27 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts - swap two int.
+ * @str: an integer from main.c.
+ * Return: returns 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
