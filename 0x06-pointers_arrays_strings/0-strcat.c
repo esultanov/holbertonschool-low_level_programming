8f743aa766ff71bcabae554862fe651ce1684043
Elnur Sultanov <esultanov6@gmail.com>
2019-10-03 16:49:22 +0000

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strtcat - concatenate string.
+ * @dest: string.
+ * @src: source string.
+ * Return: string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dst, sr;
+
+	for (dst = 0; dest[dst] != '\0'; dst++)
+		;
+	for (sr = 0; src[sr] != '\0'; sr++)
+	{
+		dest[dst] = src[sr];
+		dst++;
+	}
+	dest[dst] = '\0';
+
+	return (dest);
+}
