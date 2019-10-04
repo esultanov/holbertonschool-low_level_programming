00343a833c408161dfa9c5bce6f348032979897a
Elnur Sultanov <esultanov6@gmail.com>
2019-10-04 03:00:30 +0000

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * reverse_array - rev integers.
+ * @a: ints.
+ * @n: num of elms.
+ */
+void reverse_array(int *a, int n)
+{
+	int elm, rev;
+
+	for (elm = 0; elm < (n / 2); elm++)
+	{
+		rev = a[elm];
+		a[elm] = a[n - elm -1];
+		a[n - elm - 1] = rev;
+	}
+}
