bc380a33ac8f8175b18c9fa469010ed4c2b41e91
Elnur Sultanov <esultanov6@gmail.com>
2019-10-07 21:21:28 +0000

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memset - fills memory with const byte
+ * @n: first bytes.
+ * @s: pointed memory erea.
+ * @b: const byte.
+ * Return: pointer to the s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int test = 0; n > 0; n--, test++)
+	{
+		s[test] = b;
+	}
+	return (s);
+}
