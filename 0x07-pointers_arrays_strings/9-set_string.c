1668a4a3f6ba03094403f35e94aa2fb4fa324e84
Elnur Sultanov <esultanov6@gmail.com>
2019-10-08 03:54:42 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - gets lenght of str.
+ * @s: return number of bytes from s.
+ * @to: size of int.
+ * Return: pointer to char.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
