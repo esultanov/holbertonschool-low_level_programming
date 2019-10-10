55155afd215962e4ee8ac482d340456f03b7e4f8
Elnur Sultanov <esultanov6@gmail.com>
2019-10-10 08:11:41 +0000

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print 50 fibonacci numbers.
+ * Return: return 0.
+ */
+int main(void)
+{
+	long f = 0;
+	long i = 1;
+	int c;
+
+	for (c = 1; c <= 50; c++)
+	{
+		i = i + f;
+		f = i - f;
+		printf("%ld", i);
+		if (c < 50)
+			printf(", ");
+	}
+	putchar('\n');
+}
