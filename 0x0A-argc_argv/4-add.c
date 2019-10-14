2aebd68b9cda791ea040feb58d68a7f3d6b82088
Elnur Sultanov <esultanov6@gmail.com>
2019-10-14 03:57:45 +0000

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply nums..
+ * @argc: count.
+ * @argv: arguments.
+ * Return: 1 or 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n, t;
+	char *p;
+
+	t = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			n = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += n;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", t);
+	return (0);
+}
