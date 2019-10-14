d3eda21b12270852d699cbf75ea255b48c7360f3
Elnur Sultanov <esultanov6@gmail.com>
2019-10-14 03:46:12 +0000

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - whats my name.
+ * @argc: count.
+ * @argv: arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
