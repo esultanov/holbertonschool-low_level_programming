da6b3395f78b0fc6c8bb552552cb924767cfab5f
Elnur Sultanov <esultanov6@gmail.com>
2019-10-21 05:33:50 +0000

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,15 @@
 char *leet(char *str)
 {
 	int a, b;
-	char l[11] = "aAeEoOtTlL";
-	char d[11] = "4433007711";
+	char letters[11] = "aAeEoOtTlL";
+	char digits[11] = "4433007711";
 
 	for (a = 0; str[a] != '\0'; a++)
 	{
-		for (b = 0; l[b] != '\0'; b++)
+		for (b = 0; letters[b] != '\0'; b++)
 		{
-			if (l[b] == str[a])
-			       str[a] = d[b];
+			if (letters[b] == str[a])
+				str[a] = digits[b];
 		}
 	}
 	return (str);
