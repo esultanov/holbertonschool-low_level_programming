97e01c502a62335691f63ef0069f99df1813376e
Elnur Sultanov <esultanov6@gmail.com>
2019-10-21 06:02:03 +0000

    8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -4,15 +4,15 @@
  * @str: string.
  * Return: always 0.
  */
-char *rot13(char *)
+char *rot13(char *str)
 {
 	int a;
-	char letters[11] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char digits[11] = "nopqrstuvwxyzabcdefghijklm";
+	char letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char digits[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (a = 0; str[a] != '\0'; a++)
 	{
-		if ((str[a] > 64 && str[a] < 91 || str[a] > 96 && str[a] < 123))
+		if ((str[a] > 64 && str[a] < 91) || (str[a] > 96 && str[a] < 123))
 		{
 			str[a] = (str[a] - 65 > 25) ?
 				digits[str[a] - 97] : letters[str[a] - 65];
