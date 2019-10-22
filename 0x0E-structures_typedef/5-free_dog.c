6243882acceeee19b6bb4d61810591204be610da
Elnur Sultanov <esultanov6@gmail.com>
2019-10-22 06:43:21 +0000

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - free dog.
+ * @d: dog name.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
