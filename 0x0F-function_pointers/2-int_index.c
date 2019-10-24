6d18d6c8f539790b2631d80041b94f9612745fc8
Elnur Sultanov <esultanov6@gmail.com>
2019-10-24 03:34:52 +0000

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - print name.
+ * @array: an array.
+ * @size: n size.
+ * @cmp: function name.
+ * Return: void.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int l;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+	for (l = 0; l < size; l++)
+	{
+		if (cmp(array[l]))
+			return (i);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTIONS_POINTERS_H
 #include <stdlib.h>
 int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
