cbfb57e3726d9d0856ddbabac7a244291b278f01
Elnur Sultanov <esultanov6@gmail.com>
2019-10-28 03:51:26 +0000

    init

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,6 +11,11 @@ void _printstr(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
 
+/**
+ * struct checker - strucktur for checker.
+ * @type: points to type.
+ * @f: pointer.
+ */
 typedef struct checker
 {
 	char *type;
