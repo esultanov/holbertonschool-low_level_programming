bdea54610cfcc58770e2f56d0f9b04cd9dbd06ff
Elnur Sultanov <esultanov6@gmail.com>
2019-11-08 03:17:36 +0000

    0-binto uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - bin to int.
+ * @b: char.
+ * Return: decimal.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec;
+
+	dec = 0;
+	if (b == NULL)
+		return (0);
+	for (; *b != '\0'; b++)
+	{
+		if (*b != 49 && *b != 48)
+			return (0);
+		dec = 2 * dec + (*b - 48);
+		b++;
+	}
+	return (dec);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,10 @@
+[Holberton School of software engineering](https://www.holbertonschool.com)
+
+## Week 8 - C - Bit manipulation
+
+### In this project learning objectives are:
+
+#### General
+
+- Look for the right source of information without too much help
+- How to manipulate bits and use bitwise operators
