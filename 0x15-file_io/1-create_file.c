6a0fc561bd02c8fdacaa2e1c18190c1a6c032b49
Elnur Sultanov <esultanov6@gmail.com>
2019-11-19 01:47:26 +0000

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: is the name of the file to create.
+ * @text_content: is a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, st, count;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, OCREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (count = 0; text_content[count] != '\0'; count++)
+			;
+		st = write(fd, text_content, count);
+		if (st == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
