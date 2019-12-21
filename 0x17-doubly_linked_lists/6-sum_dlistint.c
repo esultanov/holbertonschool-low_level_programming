0a69dafb4465a0717cf6262dac8594b58ffbcd07
Elnur Sultanov <esultanov6@gmail.com>
2019-12-21 23:39:12 +0000

    6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns sum of n's.
+ * @head: pointer to list.
+ * Return: SUM or 0.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+
+	dlistint_t *temp;
+	unsigned int i;
+
+	temp = head;
+	for (i = 0; temp; temp = temp->next)
+		i = i + temp->n;
+	return (i);
+}
