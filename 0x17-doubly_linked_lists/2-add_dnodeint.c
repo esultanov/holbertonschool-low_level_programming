ea09e4688970606e3cf0654b79f87642d0b4f71f
Elnur Sultanov <esultanov6@gmail.com>
2019-12-21 22:52:24 +0000

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	if (temp == NULL)
-		new->next == NULL;
+		new->next = NULL;
 	else
 	{
 		while (temp->prev)
