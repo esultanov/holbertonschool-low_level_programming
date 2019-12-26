ab8df42223c070c1a6693934b481f8aa3385ae44
Elnur Sultanov <esultanov6@gmail.com>
2019-12-26 06:47:37 +0000

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints number of elements in a list_t list.
+ * @h: list.
+ * Return: the number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *ptr;
+	size_t i;
+
+	i = 0;
+	ptr = h;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
