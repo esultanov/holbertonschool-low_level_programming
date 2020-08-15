b068a45f7046a587df614b03e9479e17bc8a3464
Elnur Sultanov <esultanov6@gmail.com>
2020-08-15 08:17:33 +0000

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,22 +11,26 @@
 int **alloc_grid(int width, int height)
 {
 	int **arey;
-	int i, n;
+	int i, n, m, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arey = malloc(width * sizeof(int *));
-	for (i = 0; i < width; i++)
-		arey[i] = malloc(height * sizeof(int));
-	if (arey)
+	arey = malloc(height * sizeof(int *));
+	if (arey == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < width; i++)
+		arey[i] = malloc(width * sizeof(int));
+		if (arey[i] == NULL)
 		{
-			for (n = 0; n < height; n++)
-				arey[i][n] = 0;
+			for (m = i; m >= 0; m--)
+				free(arey[m]);
+			free(arey);
+			return (NULL);
 		}
 	}
-	else
-		return (NULL);
+	for (k = 0; k < height; k++)
+		for (n = 0; n < width; n++)
+			arey[k][n] = 0;
 	return (arey);
 }
